c9a63171671a72abb1b70e615c79b5dfa50cbe99
Md Shamuel Molla <shamuel@Mds-MacBook-Pro.local>
2026-03-07 22:09:05 +0600

    deployment in single api and run from root folder not from frontend or backend

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,10 @@
 import axios from "axios";
 
-const axiosInstance  =axios.create({
-    baseURL:"http://localhost:5001/api/notes"
-})
-export default axiosInstance;
+// in production, there's no localhost so we have to make this dynamic
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api/notes" : "/api/notes";
+
+const api = axios.create({
+  baseURL: BASE_URL,
+});
+
+export default api;
